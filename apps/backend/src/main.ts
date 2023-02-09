5a4c4d24d4d83c71f2d872548f63ec2735579d1c
ilya53nov <ilya53nov@yandex.ru>
2023-02-09 22:09:19 +0300

    Рефакторинг

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -11,7 +11,7 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({credentials: true, origin: "http://localhost:3000"});
+  app.enableCors({credentials: true, origin: "http://localhost:4200"});
 
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
